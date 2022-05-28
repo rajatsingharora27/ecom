0f0c0972a8e0418db600ec8d10d3bed96eb340c7
rajat <rajatsingharora27@gmail.com>
2022-05-28 12:00:22 +0530

    added controller JS file for the user

---
+++ backend/controller/user.js
@@ -0,0 +1,26 @@
+const CryptoJS = require('crypto-js');
+const User = require('../models/users');
+
+
+//Update Controller
+module.exports.updateUser =async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}; 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,29 +1,12 @@
 const router = require('express').Router();
-const User = require('../models/users');
+const {updateUser} = require('../controller/user');
 const {verifyTokenAndAuthorization} = require('./verifyToken');
-const CryptoJS = require('crypto-js');
+
 
 //UPDATE
-router.put("/:id", verifyTokenAndAuthorization,async (req, res) => {
-    if (req.body.password) {
-        req.body.password = CryptoJS.AES.encrypt(
-            req.body.password,
-            process.env.PASS_SEC
-        ).toString();
-    }
+router.get("/:id",verifyTokenAndAuthorization,updateUser);
 
-    try {
-        const updatedUser = await User.findByIdAndUpdate(
-            req.params.id,
-            {
-                $set: req.body,
-            },
-            { new: true }
-        );
-        res.status(200).json(updatedUser);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+
+
 
 module.exports = router;
